7fbf87434ca88a594b7a986771beab3888f70e1f
Steven Winfield <steven.winfield@cantab.net>
2017-05-19 00:43:44 +0100

    Added rendering test

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from vector import Vector
 from ray import Ray
+from primitives import Sphere
+from camera import Camera, Orthographic
+from scene import Scene
+from PyQt5.QtGui import QImage, QPixmap, QColor
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtCore import Qt
 
 
 x = Vector(1.0, 2.0, 3.0)
@@ -23,8 +29,47 @@ print(-z)
 
 # Ray tests
 
-eye = Vector(0.0, 0.0, -5.0)
-forward = Vector(0.0, 0.0, 1.0)
+app = QApplication([])
 
-r = Ray(eye, forward)
-print(list(r.points(3.0, 10.0)))
+eye = Vector(0.0, -2.0, 0.0)
+forward = Vector(0.0, 1.0, 0.0)
+up = Vector(0.0, 0.0, 1.0)
+
+camera = Orthographic(eye, up, forward, 3.0, 3.0)
+
+scene = Scene(600, 600, camera)
+scene.objects.append(Sphere(Vector(-0.5, 2.0, -0.5), 0.6))
+scene.objects.append(Sphere(Vector(0.5, 2.0, 0.5), 0.6))
+scene.objects.append(Sphere(Vector(-0.5, 2.0, 0.5), 0.6))
+scene.objects.append(Sphere(Vector(0.5, 2.0, -0.5), 0.6))
+image = scene.render()
+
+pixmap = QPixmap.fromImage(image)
+label = QLabel(pixmap=pixmap)
+label.show()
+
+app.exec_()
+
+
+"""
+r = Ray(eye, forward, normalize=True)
+
+#print(list(r.points(3.0, 10.0)))
+
+s = Sphere(Vector(0.0, 0.0, 0.0), 1.0)
+
+distance, normal = s.intersection(r)
+intersection = r.point(distance)
+print(distance, intersection, normal)
+"""
+"""
+from random import random
+for _ in range(100):
+    x = Vector(random(), random(), random())
+    y = Vector(random(), random(), random())
+    z = x.cross(y)
+    print(x @ z, y @ z)
+"""
+#print(Vector(1.0, 0.0, 0.0).cross(Vector(0.0, 1.0, 0.0)))
+
+# print("\n".join(str(point) for point in r.points(0.0, 5.0, 0.1)))
