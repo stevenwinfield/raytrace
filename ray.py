b9aa800fdce86f59ac320db0cc3a863da8a0898f
Steven Winfield <steven.winfield@cantab.net>
2017-06-17 20:30:41 +0100

    Add min/max distance, __neg__, docstrings

--- ray.py
+++ ray.py
@@ -1,8 +1,10 @@
 """
-ray.py
+ray.
 
 Defines the Ray class
 """
+
+from .constants import INFINITY
 
 
 class Ray:
@@ -18,15 +20,20 @@ class Ray:
         the resultant point.
     """
 
-    def __init__(self, source, direction, normalize=False):
+    def __init__(self, source, direction,
+                 min_distance=0.0, max_distance=INFINITY):
+        """Initialise a Ray."""
         self.source = source
-        self.direction = direction.normalized() if normalize else direction
+        self.direction = direction.normalized()
+        self.min_distance = min_distance
+        self.max_distance = max_distance
 
     def point(self, distance):
-        """ Return a point on this ray a given distance from its source """
+        """Return a point on this ray a given distance from its source."""
         return self.source + distance * self.direction
 
     def points(self, start, end, step=1.0):
+        """Generate points along this ray."""
         distance = start
         current = self.point(start)
         delta = step * self.direction
@@ -38,5 +45,15 @@ class Ray:
             current = current + delta  # construct a new vector
 
     def __repr__(self):
+        """Nice representation of a Ray."""
         return "{}(source={}, direction={})".format(type(self).__name__,
-                                               self.source, self.direction)
+                                                    self.source,
+                                                    self.direction)
+
+    def __neg__(self):
+        """Negate this ray.
+
+        Return a ray that has the same source but the opposite direction.
+        """
+        return type(self)(self.source, -self.direction,
+                          self.min_distance, self.max_distance)
