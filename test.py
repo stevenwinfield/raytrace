187ce6118f8eb46c64b83d23cb150123fac50234
Steven Winfield <steven.winfield@cantab.net>
2017-06-23 00:22:39 +0100

    Add docstrings to keep lint happy.

--- test.py
+++ test.py
@@ -8,10 +8,12 @@ from unittest import TestCase, main
 
 from .vector import Vector
 
+
 class Test_1_Vector(TestCase):
-    """Simple unit tests for the Vector class"""
+    """Simple unit tests for the Vector class."""
 
     def test_equality(self):
+        """Test equality."""
         x = Vector(1.0, 2.0, 3.0)
         y = Vector(1.0, 2.0, 3.0)
         z = Vector(2.0, 2.0, 3.0)
@@ -19,6 +21,7 @@ class Test_1_Vector(TestCase):
         self.assertNotEqual(x, z)
 
     def test_addition(self):
+        """Test addition."""
         x = Vector(1.0, 0.0, 0.0)
         y = Vector(0.0, 2.0, 0.0)
         result = Vector(1.0, 2.0, 0.0)
@@ -28,6 +31,7 @@ class Test_1_Vector(TestCase):
         self.assertEqual(x, result)
 
     def test_subtraction(self):
+        """Test subtraction."""
         x = Vector(1.0, 0.0, 0.0)
         y = Vector(0.0, 2.0, 0.0)
         result = Vector(1.0, -2.0, 0.0)
@@ -37,6 +41,7 @@ class Test_1_Vector(TestCase):
         self.assertEqual(x, result)
 
     def test_multiplication(self):
+        """Test multiplication."""
         x = Vector(1.0, 2.0, 3.0)
         m = 3.0
         result = Vector(3.0, 6.0, 9.0)
@@ -47,6 +52,7 @@ class Test_1_Vector(TestCase):
         self.assertEqual(x, result)
 
     def test_division(self):
+        """Test division."""
         x = Vector(1.0, 2.0, 3.0)
         d = 2.0
         result = Vector(0.5, 1.0, 1.5)
@@ -56,16 +62,19 @@ class Test_1_Vector(TestCase):
         self.assertEqual(x, result)
 
     def test_negation(self):
+        """Test negation."""
         x = Vector(3.0, 4.0, 12.0)
         result = Vector(-3.0, -4.0, -12.0)
         self.assertEqual(-x, result)
 
     def test_norm(self):
+        """Test norm/norm2."""
         x = Vector(3.0, 4.0, 12.0)
         self.assertEqual(x.norm2(), 169.0)
         self.assertEqual(x.norm(), 13.0)
 
     def test_dot_product(self):
+        """Test dot product."""
         x = Vector(1.0, 2.0, 3.0)
         y = Vector(2.0, -3.0, 5.0)
         result = 11.0
@@ -73,6 +82,7 @@ class Test_1_Vector(TestCase):
         self.assertEqual(y @ x, result)
 
     def test_cross_product(self):
+        """Test cross product."""
         x = Vector(1.0, 0.0, 0.0)
         y = Vector(0.0, 1.0, 0.0)
         result = Vector(0.0, 0.0, 1.0)
@@ -80,16 +90,22 @@ class Test_1_Vector(TestCase):
         self.assertEqual(y.cross(x), -result)
 
     def test_normalize(self):
+        """Test normalize(d)."""
         x = Vector(2.0, 3.0, 4.0)
         self.assertEqual(x.normalized().norm2(), 1.0)
         x.normalize()
         self.assertEqual(x.norm2(), 1.0)
 
     def test_reflection(self):
+        """Test reflection."""
         x = Vector(1.0, 1.0, 0.0)
         n = Vector(1.0, 0.0, 0.0)
         result = Vector(-1.0, 1.0, 0.0)
         self.assertEqual(x.reflected(n), result)
+
+
+class Test_2_Ray(TestCase):
+    """Test the Ray class."""
 
 
 def run():
